ab75dc6a329d1853cdf23991865bdcedfc8ecc24
Hayat <hayatjahangir0@gmail.com>
2024-12-06 19:54:02 +0530

    setup as sequence

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app= express();
-const authRouter = require('./router/authRouter');
+const authRouter = require('./authRouter');
 app.use(express.json());
 
 
