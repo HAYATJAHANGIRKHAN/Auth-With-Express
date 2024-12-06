9e8afe0dc8d2f6bd3ffe33bc3909743f47378250
Hayat <hayatjahangir0@gmail.com>
2024-12-06 20:17:36 +0530

    create confiq folder for the database.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app= express();
 const authRouter = require('./authRouter');
+
+const databaseconnect = require('./confiq/databaseconfiq');
+databaseconnect();
 app.use(express.json());
 
 
