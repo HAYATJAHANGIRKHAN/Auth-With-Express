54e1f32fd554b36207d2447e7f430d6f6712547c
Hayat <hayatjahangir0@gmail.com>
2024-12-06 19:08:38 +0530

    create router and controller with authentication

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app= express();
-
-
+const authRouter = require('./router/authRouter');
+app.use('/api/auth/', authRouter);
 app.use('/', (req,res) => {
 res.status(200).json({ data: '  JWTAUTH Server --server updated hayat jahangir khan'});
 });
