3cf1f65d07b8f9b61e9472643471bbd9b501f147
Hayat <hayatjahangir0@gmail.com>
2024-12-06 19:19:11 +0530

    add  data in app.js and router

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app= express();
 const authRouter = require('./router/authRouter');
+app.use(express.json());
+
+
 app.use('/api/auth/', authRouter);
 app.use('/', (req,res) => {
 res.status(200).json({ data: '  JWTAUTH Server --server updated hayat jahangir khan'});
--- backend/router/controller/authcontroller.js
+++ backend/router/controller/authcontroller.js
@@ -1,7 +1,14 @@
 const signup = (req,res,next) =>{ 
+    const {name, email, password, confirmPassword}=req.body;
+    console.log(ame, email, password, confirmPassword);
+  return res.status(200).json({
+    succuess: true,
+    data:{
 
+    }
+  })
 }
 
 module.exports ={
-    
+  signup
 }
