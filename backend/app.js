55c919fdff26c0e1a0108746890a49e24826e830
Hayat <hayatjahangir0@gmail.com>
2024-12-06 18:55:36 +0530

    setup basic server

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app= express();
 
 
-app.use
+app.use('/', (req,res) => {
+res.status(200).json({ data: '  JWTAUTH Server --server updated hayat jahangir khan'});
+});
+
+module.exports =app;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,7 @@
-const  PORT = process.env.PORT || 5000;
+const  PORT = process.env.PORT || 5001;
+
+const app= require('./app');
+
+app.listen(PORT,()=>{
+console.log(`Server is listening at http://localhost: ${PORT}`);
+});
