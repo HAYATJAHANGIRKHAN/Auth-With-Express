8d0a6c3472434d97f452861c752023f9c279535f
Hayat <hayatjahangir0@gmail.com>
2024-12-07 11:13:03 +0530

    update in auth controller with connected with database

--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -1,14 +1,38 @@
-const signup = (req,res,next) =>{ 
-    const {name, email, password, confirmPassword}=req.body;
-    console.log(ame, email, password, confirmPassword);
-  return res.status(200).json({
-    succuess: true,
-    data:{
+const signup = async (req, res, next) => {
+  const { name, email, password, confirmPassword } = req.body;
+  console.log(name, email, password, confirmPassword);
+  if(!email || !password || !confirmPassword || !name) {
+    return res.status(400).json({
+      success: false,
+      message: 'All fields are required',
+    });
+  }
 
+  try {
+    const userInfo = userModel(req.body);
+    const result = await userInfo.save();
+
+
+    return res.status(200).json({
+      succuess: true,
+      data: result,
+    });
+
+  } catch (e) {
+    if (e.code===11000){
+     return res.status(400).json({
+     success: false,
+     message: 'Account already exists with provided email',
+     });
     }
-  })
+   return res.status(500).json({
+    success: false,
+    message: e.message,
+  });
+  }
+
 }
 
-module.exports ={
+module.exports = {
   signup
 }
